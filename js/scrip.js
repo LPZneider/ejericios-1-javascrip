ecbe79395af2bc283190cff7e75f50d7f4cd1cdd
Neider Lopez <lpzneider@gmail.com>
2022-10-24 00:03:13 -0500

    ejercicios resueltos del 1-11

--- js/scrip.js
+++ js/scrip.js
@@ -71,7 +71,7 @@ const invertirC = (cadena) => {
     texto = texto.split("").reverse().join("");
     console.log(texto);
   } else {
-    console.log("error tipo de dato incorrecto");
+    console.warn("error tipo de dato incorrecto");
   }
 };
 
@@ -82,60 +82,88 @@ const invertirC = (cadena) => {
 
 */
 const contar = (palabra, buscar) => {
-  palabra = palabra.split(" ");
-  let cont = 0;
-  for (let i = 0; i <= palabra.length; i++) {
-    if (palabra[i] === buscar) {
-      cont += 1;
+  if (typeof palabra === "string" && buscar != "") {
+    palabra = palabra.split(" ");
+    let cont = 0;
+    for (let i = 0; i <= palabra.length; i++) {
+      if (palabra[i] === buscar) {
+        cont += 1;
+      }
     }
+    console.log(`la cadena tiene la palabra "${buscar}" ${cont} veces`);
+  } else {
+    console.log("error tipo de dato incorrecto");
   }
-  console.log(cont);
-  // console.log(palabra)
-  // console.log(cont)
 };
 // contar("Cuando ofrecemos un un área de un texto puede darse el caso", "un");
 
 /* 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true. */
 const validar = (palabra) => {
   if (typeof palabra === "string" && palabra != "") {
-    let oldPalabra = palabra.split("").reverse().join("");
+    let oldPalabra = palabra.split("").reverse().join("").toLowerCase();
+    palabra = palabra.toLowerCase();
     console.log(`${oldPalabra}` === palabra);
   } else {
     console.log("error tipo de dato incorrecto");
   }
 };
-// validar("reconocer");
+// validar("reconoceR");
 
 /*  8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5. */
 
-const eliminar = (texto,palabra) => {
-  if (typeof palabra === "string" && texto != "" && typeof texto === "string" ) {
-
+const eliminar = (texto, palabra) => {
+  if (typeof palabra === "string" && texto != "" && typeof texto === "string") {
     // let expReg = new RegExp(palabra, "ig");
     // texto = texto.replace(/(xyz)/gm , "")
-    texto = texto.split(palabra).join("")
-    console.log(texto)
-
-
-
-
-
-
-
-
+    texto = texto.split(palabra).join("");
+    console.log(texto);
   } else {
     console.log("error tipo de dato incorrecto");
   }
+};
+
+// eliminar("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
+
+/* 9) Programa una función que obtenga un numero aleatorio entre 501 y 600 */
+
+const aleatorio = () =>
+  console.log(Math.round(Math.random() * (600 - 501) +501));
+
+// aleatorio()
+
+/* 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true  */
+
+const capicúa =(numero) =>{
+  if(!numero) return console.error("no ingresaste un numero")
+  let myInt = numero;
+  let myFunc = num => Number(num);
+  let intArr = Array.from(String(myInt), myFunc)
+  intArr = intArr.reverse().join("")
+  myInt = myInt.toString()
+  console.log(myInt === intArr);
+
+  
 }
+// capicúa(2002);
 
-eliminar("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz")
+const capicúa2 = (numero) =>{
+  if(!numero) return console.error("no ingresaste un numero");
+  let varia = String(numero)
+  let result = varia.split("").reverse().join("")
+  console.info(varia === result)
+}
+// capicúa2(23432)
 
-/* let cadena = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem fuga cupiditate dolores saepe, praesentium sit eaque lorem recusandae id sapiente similique, laudantium voluptatum perferendis ea iure ad odio doloremque earum voluptate.";
 
-     let expReg = new RegExp("lorem", "ig");
-     console.log(expReg.test(cadena));
-     console.log(expReg.exec(cadena));
 
-     let expReg2 = /lorem{1,}/ig;
-     console.log(expReg2.test(cadena));
-     console.log(expReg2.exec(cadena)); */
+/* 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120 */
+
+const factorial = (numero) =>{
+  if(!numero) return console.error("no ingresaste un numero");
+  let num = numero;
+  for(let i= 1; i<numero; i++){
+    num *= i
+  }
+  console.log(num)
+}
+factorial(4)
