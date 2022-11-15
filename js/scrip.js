7f1d83633a3acb778b99a7bbcb66c58460a781db
Neider Lopez <lpzneider@gmail.com>
2022-11-14 23:44:14 -0500

    resolviendo ejercicio 18

--- js/scrip.js
+++ js/scrip.js
@@ -256,7 +256,14 @@ const fechaAnio = (fecha) => {
 // fechaAnio("31/02/2004")
 
 /* 18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5 */
+const contarVocalesConsonantes = (cadena = "") => {
+  if(cadena === "") return console.warn("no ingresaste una  cadena")
 
+  if(typeof cadena !== 'string') return console.error(`el valor ${cadena}, NO es un String`)
+
+
+}
+contarVocalesConsonantes();
 /* 19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero */
 
 /* 20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero */
